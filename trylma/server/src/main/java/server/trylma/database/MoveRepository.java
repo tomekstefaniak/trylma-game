2ebc3d1ef7fc11db1d5494574e6e183e0fdf38b0
Błażej Pawluk <bpawluk05@gmail.com>
2025-02-01 14:02:19 +0100

    poprawka do wczytywania gry

--- trylma/server/src/main/java/server/trylma/database/MoveRepository.java
+++ trylma/server/src/main/java/server/trylma/database/MoveRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MoveRepository extends JpaRepository<Move, Integer> {
-	@Query("SELECT m.* FROM Move m WHERE m.port =: port AND m.game =: game")
+	@Query("SELECT m.id, m.port, m.game, m.board FROM Move m WHERE m.port =: port AND m.game =: game")
 	ArrayList<Move> findGame(@Param("port") Integer port, @Param("game") Integer game);
 }
